591b032c7ab91801b438066c2c8fbae74f5412f4
Saheme <roj_t@mail.ru>
2022-05-19 11:06:27 +0300

    первое приложение

---
+++ src/Part1/MyFirstApp1.java
@@ -0,0 +1,8 @@
+package Part1;
+
+public class MyFirstApp1 {
+    public static void main(String[] args) {
+        System.out.println("Я управляю ");
+        System.out.println("миром");
+    }
+}
