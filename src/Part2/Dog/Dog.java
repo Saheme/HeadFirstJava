9c1fabfe7fbf1d8ee38b906c0485f1ae37afcdf8
Saheme <roj_t@mail.ru>
2022-05-22 01:43:56 +0300

    all tasks

---
+++ src/Part2/Dog/Dog.java
@@ -0,0 +1,12 @@
+package Part2.Dog;
+
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark(){
+        System.out.println(name + " " + "Gav Gav");
+    }
+
+}
---
+++ src/Part2/Dog/DogTest.java
@@ -0,0 +1,10 @@
+package Part2.Dog;
+
+public class DogTest {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.name = "Toby";
+        d.size = 12;
+        d.bark();
+    }
+}
